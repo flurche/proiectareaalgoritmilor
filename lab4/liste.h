e7dab9084a117a7083eaa7628ebe2fdfb29dfecf
flurche <sflurche@yahoo.com>
2023-03-22 03:12:26 +0200

    created test for default functions

---
+++ lab4/liste.h
@@ -0,0 +1,16 @@
+#ifndef MYLIB_H
+#define MYLIB_H
+
+typedef int Data;
+
+typedef struct Node {
+  Data val;
+  struct Node* next;
+
+} Node;
+
+void addAtBeginning(Node **head, Data v);
+void addAtEnd(Node **head, Data v);
+void display(Node *head);
+
+#endif
---
+++ lab4/test.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include "liste.h"
+
+
+int main() {
+  // Create an empty linked list
+  Node *head = NULL;
+
+  // Add some elements to the linked list
+  addAtEnd(&head, 1);
+  addAtEnd(&head, 2);
+  addAtEnd(&head, 3);
+
+  // Display the linked list
+  printf("Linked list: ");
+  display(head);
+
+  // Add an element to the beginning of the linked list
+  addAtBeginning(&head, 0);
+
+  // Display the linked list again
+  printf("Linked list after adding an element to the beginning: ");
+  display(head);
+  return 0;
+}
